9465cffba997fb7bf1319fdcd9fbe4f80d3cf137
hlemont <76568223+hlemont@users.noreply.github.com>
2025-12-16 16:07:37 +0900

    Move from __init__.py to __main__.py

--- src/mcp_server_obsidian_omnisearch/__main__.py
+++ src/mcp_server_obsidian_omnisearch/__main__.py
@@ -1,5 +1,17 @@
-# __main__.py
+from .server import serve
 
-from mcp_server_obsidian_omnisearch import main
 
-main()
+def main():
+    import sys
+    import asyncio
+
+    if len(sys.argv) < 2:
+        print("Usage: python server.py <obsidian_vault_path>")
+        sys.exit(1)
+
+    obsidian_vault_path = sys.argv[1]
+    asyncio.run(serve(obsidian_vault_path))
+
+
+if __name__ == "__main__":
+    main()
